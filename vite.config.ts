cfc19c9d4e09cefe205a80e48e7417b26f5f4c15
Erik Hellman <erik.hellman@husqvarnagroup.com>
2025-07-11 19:47:00 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/friendly-local-chatbot/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
